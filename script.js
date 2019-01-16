6aa06c3ca0659b2ddf84b8d89e06a43a2307a7d0
Artemy Kairyak <artemykairyak@yandex.com>
2019-01-17 01:05:59 +0400

    added form validation, some fixes

--- script.js
+++ script.js
@@ -16,13 +16,20 @@ window.onload = function(){
 	};
 
     $('.messages').slick({
-      infinite: false,
+      infinite: true,
       slidesToShow: 1,
       slidesToScroll: 2,
       vertical: true,
       adaptiveHeight: true,
       prevArrow: $('#arrow-up'),
-      nextArrow: $('#arrow-down')
+      nextArrow: $('#arrow-down'),
+       responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToScroll: 1
+      }
+    }]
     });
 
     $('.team-members').slick({
@@ -71,45 +78,101 @@ window.onload = function(){
 
     $('.burger').on('click', event => {
     	$('nav ul').addClass('hidden-menu');
-    	$('nav').slideToggle('fast');
+    	$('nav').toggle('fast');
     })
 
     $('input[name="send-btn"]').on('click', function(e) {
     	if (!validateForm()) {
     		e.preventDefault();
-    		alert(1);
-       	}
+        
+       	} else {
+          $('span.unfilled').hide();
+          sendAJAX();
+        }
     });
 
+    function sendAJAX() {
+      $.post('/server.php', {
+        firstName: firstName.val().trim(),
+        lastName: lastName.val().trim(),
+        email: email.val().trim(),
+        phone: phone.val().trim(),
+        message: message.val().trim()
+      }, onAJAXSuccess
+    )}
+
+    function onAJAXSuccess(data) {
+      var content = '<div class="success-form"><p>Thanks!</p></div>'
+        if (!$('.form-container').is($('.success-form'))) {
+          $('.form-container').append(content);  
+          $('.success-form').animate({opacity: '1'}, 500).delay(2000).fadeOut(); 
+          for (var i = 0; i < formData.length; i++) {
+            formData[i].val('');
+          }
+        }
+    }
+
+    function isNumeric(n) {
+      return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+
+    var contactForm = $('.contact-form');
+    var firstName = $("input[name='name']");
+    var lastName = $("input[name='lastname']");
+    var email = $("input[name='email']");
+    var phone = $("input[name='phone']");
+    var message = $("textarea[name='message']");
+
+    var formData = [firstName, lastName, email, phone, message];
+
     function validateForm() {
-    	var contactForm = $('.contact-form');
-    	var firstName = $("input[name='name']");
-    	var lastName = $("input[name='lastname']");
-    	var email = $("input[name='email']").val().trim();
-    	var phone = $("input[name='phone']").val().trim();
-    	var message = $("textarea[name='message']").val().trim();
+    	
     	var errors = false;
 
     	if(firstName.val().trim().length === 0) {
     		firstName.addClass('input-error');
     		errors = true;
-    	}
+    	} else {
+        firstName.removeClass('input-error');
+      }
 
     	if(lastName.val().trim().length === 0) {  
     		lastName.addClass('input-error');
     		errors = true;
-    	}
+    	} else {
+        lastName.removeClass('input-error');
+      }
 
-    	if(email.val().trim().length > 3 && (email.val().trim().includes('@')) 
-    		&& (email.val().trim().includes('.'))) {
+    	if(email.val().trim().length < 3 || !email.val().trim().includes('@') 
+    		|| !email.val().trim().includes('.')) {
     			email.addClass('input-error');
     			errors = true;
-    	} 
+    	} else {
+        email.removeClass('input-error');
+      }
 
+      if(!isNumeric(phone.val().trim()) && phone.val().trim().length === 0) {
+        phone.addClass('input-error');
+        errors = true;
+      } else {
+        phone.removeClass('input-error');
+      }
+
+      if(message.val().trim().length === 0) {  
+        message.addClass('input-error');
+        errors = true;
+      } else {
+        message.removeClass('input-error');
+      }
 
     	if (errors) {
-    		contactForm.prepend("<span class='unfilled'>Fill this fields</span>");
+        if(!$('span').is('.unfilled')) {
+          contactForm.prepend("<span class='unfilled'>Fill this fields</span>");
+        }
+
     		return false;
     	}
+
+      return true;
     }
 };
---
+++ server.php
@@ -0,0 +1,3 @@
+<?php
+  echo "name = ".$_POST['firstName']." last name = ".$_POST['lastName']. " email = ".$_POST['email']. " phone = ".$_POST['phone']. " message = ".$_POST['message'];
+?>

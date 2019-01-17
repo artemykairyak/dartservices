a854a7a420feaf28dbbac381d3308661d98b643b
Artemy Kairyak <artemykairyak@yandex.com>
2019-01-17 21:32:45 +0400

    added team-slider

--- script.js
+++ script.js
@@ -15,101 +15,90 @@ window.onload = function(){
 		buttonLink.style.color = '';
 	};
 
-    $('.messages').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 2,
-      vertical: true,
-      adaptiveHeight: true,
-      prevArrow: $('#arrow-up'),
-      nextArrow: $('#arrow-down'),
-       responsive: [
+  $('.messages').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    vertical: true,
+    adaptiveHeight: true,
+    prevArrow: $('#arrow-up'),
+    nextArrow: $('#arrow-down'),
+    responsive: [
     {
       breakpoint: 480,
       settings: {
         slidesToScroll: 1
       }
     }]
-    });
+  });
 
-    $('.team-members').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      centerMode: true,
-    
-      arrows: false,
-      initialSlide: 1
-    });
+  $('.button').on('mousedown', event => {
+    $(event.currentTarget).addClass('btn-active');
+  });
 
+  $('.button').on('mouseup', event => {
+    $(event.currentTarget).removeClass('btn-active');
+  });
 
-	$('.button').on('mousedown', event => {
-		$(event.currentTarget).addClass('btn-active');
-	});
+  $(".services-panel .label:first").addClass("active").children(".right-icon").addClass('rotate');
+  $(".services-panel .content:not(:first)").hide();
 
-	$('.button').on('mouseup', event => {
-		$(event.currentTarget).removeClass('btn-active');
-	});
+  $(".services-panel .label").on('click', event =>{
+   $(event.currentTarget).next(".content").slideToggle("fast");
+   $(event.currentTarget).children(".right-icon").toggleClass('rotate');
+   $(event.currentTarget).toggleClass("active");         
+ });
 
-	$(".services-panel .label:first").addClass("active").children(".right-icon").addClass('rotate');
-    $(".services-panel .content:not(:first)").hide();
+  $('input[type="text"]').on('mouseenter', event => {
+   $(event.currentTarget).css({borderColor: '#1de9b6'});
+ })
 
-    $(".services-panel .label").on('click', event =>{
-        	$(event.currentTarget).next(".content").slideToggle("fast");
-        	$(event.currentTarget).children(".right-icon").toggleClass('rotate');
-        	$(event.currentTarget).toggleClass("active");         
-     });
+  $('input[type="text"]').on('mouseleave', event => {
+   $(event.currentTarget).css({borderColor: ''});
+ })
 
-    $('input[type="text"]').on('mouseenter', event => {
-    	$(event.currentTarget).css({borderColor: '#1de9b6'});
-    })
+  $('textarea').on('mouseenter', event => {
+   $(event.currentTarget).css({borderColor: '#1de9b6'});
+ })
 
-    $('input[type="text"]').on('mouseleave', event => {
-    	$(event.currentTarget).css({borderColor: ''});
-    })
+  $('textarea').on('mouseleave', event => {
+   $(event.currentTarget).css({borderColor: ''});
+ })
 
-    $('textarea').on('mouseenter', event => {
-    	$(event.currentTarget).css({borderColor: '#1de9b6'});
-    })
+  $('.burger').on('click', event => {
+   $('nav ul').addClass('hidden-menu');
+   $('nav').toggle('fast');
+ })
 
-    $('textarea').on('mouseleave', event => {
-    	$(event.currentTarget).css({borderColor: ''});
-    })
+  $('input[name="send-btn"]').on('click', function(e) {
+   if (!validateForm()) {
+    e.preventDefault();
 
-    $('.burger').on('click', event => {
-    	$('nav ul').addClass('hidden-menu');
-    	$('nav').toggle('fast');
-    })
+  } else {
+    $('span.unfilled').hide();
+    sendAJAX();
+  }
+});
 
-    $('input[name="send-btn"]').on('click', function(e) {
-    	if (!validateForm()) {
-    		e.preventDefault();
-        
-       	} else {
-          $('span.unfilled').hide();
-          sendAJAX();
-        }
-    });
-
-    function sendAJAX() {
-      $.post('/server.php', {
-        firstName: firstName.val().trim(),
-        lastName: lastName.val().trim(),
-        email: email.val().trim(),
-        phone: phone.val().trim(),
-        message: message.val().trim()
-      }, onAJAXSuccess
+  function sendAJAX() {
+    $.post('/server.php', {
+      firstName: firstName.val().trim(),
+      lastName: lastName.val().trim(),
+      email: email.val().trim(),
+      phone: phone.val().trim(),
+      message: message.val().trim()
+    }, onAJAXSuccess
     )}
 
     function onAJAXSuccess(data) {
       var content = '<div class="success-form"><p>Thanks!</p></div>'
-        if (!$('.form-container').is($('.success-form'))) {
-          $('.form-container').append(content);  
-          $('.success-form').animate({opacity: '1'}, 500).delay(2000).fadeOut(); 
-          for (var i = 0; i < formData.length; i++) {
-            formData[i].val('');
-          }
+      if (!$('.form-container').is($('.success-form'))) {
+        $('.form-container').append(content);  
+        $('.success-form').animate({opacity: '1'}, 500).delay(2000).fadeOut(); 
+        for (var i = 0; i < formData.length; i++) {
+          formData[i].val('');
         }
+      }
     }
 
     function isNumeric(n) {
@@ -126,7 +115,6 @@ window.onload = function(){
     var formData = [firstName, lastName, email, phone, message];
 
     function validateForm() {
-    	
     	var errors = false;
 
     	if(firstName.val().trim().length === 0) {
@@ -136,43 +124,123 @@ window.onload = function(){
         firstName.removeClass('input-error');
       }
 
-    	if(lastName.val().trim().length === 0) {  
-    		lastName.addClass('input-error');
-    		errors = true;
-    	} else {
+      if(lastName.val().trim().length === 0) {  
+        lastName.addClass('input-error');
+        errors = true;
+      } else {
         lastName.removeClass('input-error');
       }
 
-    	if(email.val().trim().length < 3 || !email.val().trim().includes('@') 
-    		|| !email.val().trim().includes('.')) {
-    			email.addClass('input-error');
-    			errors = true;
-    	} else {
-        email.removeClass('input-error');
-      }
+      if(email.val().trim().length < 3 || !email.val().trim().includes('@') 
+        || !email.val().trim().includes('.')) {
+       email.addClass('input-error');
+     errors = true;
+   } else {
+    email.removeClass('input-error');
+  }
 
-      if(!isNumeric(phone.val().trim()) && phone.val().trim().length === 0) {
-        phone.addClass('input-error');
-        errors = true;
-      } else {
-        phone.removeClass('input-error');
-      }
+  if(!isNumeric(phone.val().trim()) && phone.val().trim().length === 0) {
+    phone.addClass('input-error');
+    errors = true;
+  } else {
+    phone.removeClass('input-error');
+  }
 
-      if(message.val().trim().length === 0) {  
-        message.addClass('input-error');
-        errors = true;
-      } else {
-        message.removeClass('input-error');
-      }
+  if(message.val().trim().length === 0) {  
+    message.addClass('input-error');
+    errors = true;
+  } else {
+    message.removeClass('input-error');
+  }
 
-    	if (errors) {
-        if(!$('span').is('.unfilled')) {
-          contactForm.prepend("<span class='unfilled'>Fill this fields</span>");
-        }
-
-    		return false;
-    	}
-
-      return true;
+  if (errors) {
+    if(!$('span').is('.unfilled')) {
+      contactForm.prepend("<span class='unfilled'>Fill this fields</span>");
     }
+
+    return false;
+  }
+
+  return true;
+}
+
+  var teamMembers = [
+  {
+    name: 'Putin',
+    img: './img/putin.jpg', 
+    desc: '<h4 class="name">Vladimir Putin</h4><p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim cumque assumenda, praesentium, autem minus vero aperiam fugiat explicabo cupiditate dignissimos alias tenetur libero reiciendis officiis</p>'
+  },
+  {
+    name: 'Trump',
+    img: './img/tramp.jpg',
+    desc: '<h4 class="name">Donald Trump</h4><p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim cumque assumenda, praesentium, autem minus vero aperiam fugiat explicabo cupiditate dignissimos alias tenetur libero reiciendis officiis, nostrum vitae debitis eveniet quo.</p>'
+  },
+  {
+    name: 'Navalny',
+    img: './img/navalny.jpg',
+    desc: '<h4 class="name">Alexey Navalny</h4><p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim cumque assumenda, nostrum vitae debitis eveniet quo.</p>'
+  },
+  {
+    name: 'Newton',
+    img: './img/newton.jpeg',
+    desc: '<h4 class="name">Isaac Newton</h4><p class="description">Lorem ipsum dolor sit amet quo.</p>'
+  },
+  {
+    name: 'Obama',
+    img: './img/obama.jpg',
+    desc: '<h4 class="name">Barack Obama</h4><p class="description">Lorem ipsumdddddddddddddd dolor sit amet quo.</p>'
+  }]
+
+  $('.member-slide').click(onMemberClick);
+
+  hideMembers();
+
+  function hideMembers() {
+   $('.member').each(function() {
+     if ($(this).attr('data-num') < 1 || $(this).attr('data-num') > 3) {
+       $(this).css({'display' : 'none'});
+     } else {
+      $(this).css({'display' : 'initial'});
+    }
+  })  
+  }
+
+  function addSelectedMember() {
+    $('.member').each(function() {
+      if (!$(this).is('[data-num=2]')) {
+        $(this).parent().removeClass('selected-slide');
+        $(this).removeClass('memberselected');
+      } else {
+        $(this).parent().addClass('selected-slide');
+        $(this).addClass('memberselected');
+      }
+    })
+  }
+
+  function changeMemberInfo(m) {
+    for (var i = 0; i < teamMembers.length; i++) {
+      if (m === teamMembers[i].name) {
+        $('.member-info').html(teamMembers[i].desc);
+      }
+    }
+  }
+
+  function onMemberClick(e) {
+    if (!$(this).hasClass('selected-slide')) {
+      if ($(this).next().hasClass('selected-slide') && !$(this).is(':first')) {
+        $('.member').each(function() {
+          $(this).attr('data-num', +$(this).attr('data-num') + 1);
+        });
+      }
+
+      if ($(this).prev().hasClass('selected-slide') && !$(this).is(':last')) {
+        $('.member').each(function() {
+          $(this).attr('data-num', $(this).attr('data-num') - 1);
+        });        
+      }
+    }
+    hideMembers(); 
+    addSelectedMember();
+    changeMemberInfo($(this).children().attr('data-name'));
+  }
 };

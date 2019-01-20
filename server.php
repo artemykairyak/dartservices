f70f0c897afada55d4bb31dd5955920e1fcc2543
Artemy Kairyak <artemykairyak@yandex.com>
2019-01-20 13:42:54 +0400

    added prefixes and some minor fixes

--- server.php
+++ server.php
@@ -1,3 +1,3 @@
 <?php
-  echo "name = ".$_POST['firstName']." last name = ".$_POST['lastName']. " email = ".$_POST['email']. " phone = ".$_POST['phone']. " message = ".$_POST['message'];
+  echo "name = ".$_POST['firstName']." last name = ".$_POST['lastName']. " email = ".$_POST['email']. " phone = ".$_POST['phone']. " message = ".$_POST['message']. " subscribe = ".$_POST['subscribe'];
 ?>
